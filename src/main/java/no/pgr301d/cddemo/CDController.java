b67d3124f70c051901f6e9c1dca32285ca0a57d9
EC2 Default User <ec2-user@ip-172-31-34-238.eu-west-1.compute.internal>
2024-10-01 08:47:36 +0000

    workflow3

--- src/main/java/no/pgr301d/cddemo/CDController.java
+++ src/main/java/no/pgr301d/cddemo/CDController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 class CDController {
-    //Haha
+    //Hahaha
 
     @GetMapping(path = "/")
     public String hello() {
